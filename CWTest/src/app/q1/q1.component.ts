ecce7236aa6fd9fe3723f33f1d6f0e7d0c04aca4
Shubham <shiroresn@gmail.com>
2020-08-31 23:56:00 +0530

    Question 1 completed

--- CWTest/src/app/q1/q1.component.ts
+++ CWTest/src/app/q1/q1.component.ts
@@ -9,7 +9,23 @@ export class Q1Component implements OnInit {
 
   constructor() { }
 
+  charArray = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
+  color="white";
+  count=0;
+
   ngOnInit(): void {
+  }
+
+  generateRandomColor(){
+    var colorString = "#";
+    for(var i=0;i<6;i++)
+    {
+     var num =  Math.floor(Math.random() * 15);
+     colorString += this.charArray[num];
+    }
+
+    this.color=colorString;
+    this.count++;
   }
 
 }
