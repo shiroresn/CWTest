e07b1a256aa938ef4a2888b96e905a3d33ef3556
Shubham <shiroresn@gmail.com>
2020-09-01 01:19:54 +0530

    Q2 completed

--- CWTest/src/app/q2/q2.component.ts
+++ CWTest/src/app/q2/q2.component.ts
@@ -9,7 +9,51 @@ export class Q2Component implements OnInit {
 
   constructor() { }
 
+  inputArrayString="";
+  n;
+  pattern = /[0-9]/;
+  showCalculations = false;
+
+  inputArrayNumbers = new Array();
+  finalAnswer = 0;
+
   ngOnInit(): void {
+  }
+
+  onCalculate() {
+    this.inputArrayNumbers = new Array();
+    this.showCalculations = true;
+
+    var stringToArray: any[] = this.inputArrayString.split(",", 100);
+    stringToArray.forEach(element => {
+      this.inputArrayNumbers.push(+element);
+    });
+
+    this.finalAnswer = this.calculateAnswer(this.inputArrayNumbers,this.n);
+
+  }
+
+  calculateAnswer(inputArrayNumbers,n){
+    var maxSumArray:number[]=new Array();
+
+     for (var i = 0; i < inputArrayNumbers.length; i++) {
+      var tempSum=0;
+      var k =+i.toString();
+      for(var j=0;j<n;j++)
+      {   
+        try {
+          tempSum+=inputArrayNumbers[k++];
+        } catch (error) {
+          tempSum+=0;
+        }
+      }
+     
+      maxSumArray.push(tempSum);
+    }
+
+    console.log(maxSumArray);
+    
+    return maxSumArray.sort((a,b) => b - a)[0];
   }
 
 }
